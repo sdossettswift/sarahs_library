e847979731e7306ec255b1b1354e7c2a9360e771
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-07-06 15:02:29 -0500

    create book model, routes, controller, views

---
+++ sarahs_library/app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find_by id: params[:id]
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new
+    @book.title = params[:book][:title]
+    @book.author = params[:book][:author]
+    @book.price = params[:book][:price]
+    @book.photo_url = params[:book][:photo_url]
+
+    if @book.save
+      redirect_to root_path, notice: "Book Added!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find_by id: params[:id]
+  end
+
+  def update
+    @book = Book.find_by id: params[:id]
+    @book.title = params[:book][:title]
+    @book.author = params[:book][:author]
+    @book.price = params[:book][:price]
+    @book.photo_url = params[:book][:photo_url]
+
+    if @book.save
+      redirect_to root_path, notice: "Book Added!"
+    else
+      render :new
+    end
+  end
+
+  def delete
+    @book = Book.find_by id: params[:id]
+    @book.destroy
+    redirect_to root_path, notice: "Book Burned!"
+  end
+
+end
---
+++ sarahs_library/db/migrate/20160706194909_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.decimal :price
+      t.string :author
+      t.string :photo_url
+
+      t.timestamps null: false
+    end
+  end
+end
